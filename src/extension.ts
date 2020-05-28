b0c5825bcf5f62739fb40e94883ef51b9b85ac64
Axel Chalon <xaxel@protonmail.com>
2020-05-28 02:05:14 +0200

    Getting started (webview)

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,25 @@ import { setUpMarketplaceTreeView } from './views/marketplace/MarketplaceProvide
 import { setUpRuntimesTreeView } from './views/runtimes/RuntimesProvider';
 import Runtimes from './runtimes/Runtimes';
 
+const fs = require('fs');
+const path = require('path');
+
 export function activate(context: vscode.ExtensionContext) {
 	// TODO dependency management across components
+
+	const isTheia = process.env.SUBSTRATE_PLAYGROUND !== undefined;
+
+	if (isTheia || true) { // TODO as such, should maybe not be
+
+		var resultPanel = vscode.window.createWebviewPanel("welcome", "Getting started", vscode.ViewColumn.One, {enableScripts: true, enableCommandUris: true});
+		resultPanel.webview.html = fs.readFileSync(path.join(__filename, '..', '..', 'resources', 'welcome.html')).toString();
+
+		resultPanel.webview.onDidReceiveMessage(message => {
+			if (message.command === 'tour') {
+				vscode.commands.executeCommand("TheiaSubstrateExtension.tour-command");
+			}
+		}, undefined, context.subscriptions);
+	}
 
 	const runtimes = new Runtimes();
 
